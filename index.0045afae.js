797b478a473ecb2d794814f391520e272685407a
Kudese <106887354+Kudese@users.noreply.github.com>
2022-11-28 03:49:08 +0000

    Deploying to gh-pages from @ Kudese/studi_123456789@7ae25268f131adc2489e9c90b004e6e7462478c2 🚀

---
+++ index.0045afae.js
@@ -0,0 +1,2 @@
+console.log("Pig latin is cool !".split(" ").map((function(i){return i.includes("1")||i.includes("?")?i:i.split("").slice(1).join("")+i.split("")[0]+"ay"})).join(" "));
+//# sourceMappingURL=index.0045afae.js.map
---
+++ index.31ceacc0.js
@@ -0,0 +1,2 @@
+console.log("Pig latin is cool !".split(" ").map((i=>i.includes("1")||i.includes("?")?i:i.split("").slice(1).join("")+i.split("")[0]+"ay")).join(" "));
+//# sourceMappingURL=index.31ceacc0.js.map
